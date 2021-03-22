6d78f9108aa5d44bd33de05a151bfb3997b4a115
GitHub Action <action@github.com>
2021-03-22 18:06:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/exequiel222/featuretemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.exequiel222.featuretemplate.services
+
+import com.github.exequiel222.featuretemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
